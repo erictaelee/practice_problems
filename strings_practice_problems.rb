01014042d6507ce9168d4499d7ac2a8026b0f273
Eric <lee.erictae@gmail.com>
2021-02-19 22:16:51 -0600

    practice strings and put it in skill folder

---
+++ strings_practice_problems.rb
@@ -0,0 +1,71 @@
+# 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+# first_name = "Eric"
+# last_name = "Lee"
+
+# puts first_name + " " + last_name
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+# first_name = "Eric"
+# last_name = "Lee"
+
+# puts "#{first_name} #{last_name}"
+
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+# word = gets.chomp
+# if word == "marco"
+#   puts "polo"
+# end  
+
+
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+# color_1 = "red"
+# color_2 = "blue"
+# color_3 = "green"
+
+# puts color_1 + " " + color_2 + " " + color_3
+
+# 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+# color_1 = "red"
+# color_2 = "blue"
+# color_3 = "green"
+
+# puts "#{color_1} #{color_2} #{color_3}"
+
+
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+# puts "enter a name"
+# name = gets.chomp
+# if name != "Santa"
+#   puts "You're not Santa"
+# end
+
+
+# 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+# title = "Harry Potter"
+# author = "JK Rowling"
+
+# puts author + " wrote " + title
+
+# # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+# title = "Harry Potter"
+# author = "JK Rowling"
+
+# puts "#{author} wrote #{title}"
+
+
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+# puts "enter a password"
+# password = gets.chomp
+# if password == "Joshua"
+#   puts "Shall we play a game?"
+# else
+#   puts "Access denied"  
+# end
+
+
+# 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+# city_1 = "Chicago"
+# city_2 = "Aurora"
+# city_3 = "New York"
+
+# puts city_1 + city_2 + city_3
