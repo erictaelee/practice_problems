ebc6c56fb6a1be7794b3d130ed21634a500b376c
Eric <lee.erictae@gmail.com>
2021-02-28 01:20:58 -0600

    loops practice

---
+++ loops_practice_problems.rb
@@ -0,0 +1,82 @@
+# 1. Write a while loop to print the numbers 1 through 10.
+
+# number = 0
+# while number < 10
+#   number = number + 1
+#   p number
+# end
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+# index = 0
+# while index < 5
+#   puts "hello"
+#   index += 1
+# end
+
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+# while true
+# puts "enter a word (stop to quit)"
+# word = gets.chomp
+#   if word == "stop"
+#     break
+#   end  
+# end
+
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+# num = 0
+# while num < 101
+#   p num
+#   num = num + 5
+# end
+
+# 5. Write a while loop that prints the number 9000 ten times.
+# num = 9000
+# index = 0
+# while index < 10
+#   p num
+#   index += 1
+# end
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+# while true
+#   puts "enter a number"
+#   num = gets.chomp.to_i
+#   if num > 10
+#     break
+#   end
+# end
+
+
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+# num = 50
+# while num <= 70
+#   p num
+#   num = num + 1
+# end
+
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+# index = 0
+# while index < 144
+#   puts "Around the world"
+#   index += 1
+# end
+
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+# while true
+#   puts "enter a word:"
+#   word = gets.chomp.to_s
+#   if word.length > 5
+#     break
+#   end
+# end
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+num = 2
+while num <= 40
+  p num
+  num = num + 2
+end
