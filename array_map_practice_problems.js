b3c4c8b1db2ac265dcafef3f5cde8b331c4d686c
Eric <lee.erictae@gmail.com>
2021-04-04 21:29:49 -0500

    practice js and ruby

--- array_map_practice_problems.js
+++ array_map_practice_problems.js
@@ -11,3 +11,31 @@ strings.forEach(function (string) {
   newStrings.push(string.toUpperCase());
 })
 console.log(newStrings)
+
+var array = [{ name: "Alice", age: 27 }, { name: "Blane", age: 16 }];
+var newArray = [];
+array.forEach(function (name) {
+  newArray.push(name["name"])
+})
+console.log(newArray)
+
+var arr = [1, 2, 3];
+var newArr = [];
+arr.forEach(function (number) {
+  newArr.push(number + 7)
+})
+console.log(newArr)
+
+var arr = ["hello", "goodbye"];
+var newArr = [];
+arr.forEach(function (string) {
+  newArr.push(string.length)
+})
+console.log(newArr)
+
+var arr = [1, 2, 3];
+var newArr = [];
+arr.forEach(function (half) {
+  newArr.push(half / 2.0)
+})
+console.log(newArr)
--- arrays_map_practice_problems.rb
+++ arrays_map_practice_problems.rb
@@ -51,18 +51,62 @@ p new_strings
 
 #  3. Start with an array of hashes and create a new array of string values from each hash's :name key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes ["Alice", "Blane"].
+array = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+new_array = []
+array.each do |name|
+  new_array << name[:name]
+end
+p new_array
+
+array = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+p array.map {|name| name[:name]}
+
+array = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+new_array = []
+i = 0
+while i < array.length
+  new_array << array[i][:name]
+  i += 1
+end
+p new_array
+
+
 
 #  4. Start with an array of numbers and create a new array with each number plus 7.
 #     For example, [1, 2, 3] becomes [8, 9, 10].
 
+array = [1, 2, 3]
+new_array = []
+array.each do |number|
+  new_array << number + 7
+end
+p new_array
+
 #  5. Start with an array of strings and create a new array with each string's length.
 #     For example, ["hello", "goodbye"] becomes [5, 7].
+
+array = ["hello", "goodbye"]
+new_array = []
+array.each do |number|
+  new_array << number.length
+end
+p new_array
 
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
+
+array = [1, 2 ,3]
+new_array = []
+array.each do |half|
+  new_array << half / 2.0
+end
+p new_array
+
+array = [1, 2 ,3]
+p array.map {|half| half / 2.0 }
 
 #  8. Start with an array of strings and create a new array with each string's first letter only.
 #     For example, ["hello", "goodbye"] becomes ["h", "g"].
